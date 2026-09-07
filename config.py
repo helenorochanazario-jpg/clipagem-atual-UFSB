9d32ce84d551c3c79082e7c678adaa472042b799
Heleno Rocha Nazário <helenorochanazario@gmail.com>
2026-09-06 23:05:18 -0300

    Update config.py

--- config.py
+++ config.py
@@ -22,9 +22,9 @@ SEARCH_TERMS = [
 # Adicione variações (com e sem título) para melhorar a detecção, ex.:
 # "Fabrício Zanchi", "Reitor Fabrício Berton Zanchi".
 UFSB_PEOPLE = ["Fabrício Berton Zanchi", "reitor Fabrício Berton Zanchi"; "Caroline Rezende Caputo", "Caroline Caputo", "vice-reitora Caroline Caputo", "pró-reitor", "pró-reitora"
+]
     # "Fabrício Berton Zanchi",
     # "Nome Sobrenome",
-]
 
 # Idioma usado pelo pysentimiento para classificar sentimento (não altere).
 SENTIMENT_LANG = "pt"
